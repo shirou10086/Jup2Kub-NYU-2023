f5ae7bd65322399b8a1e87ff77404f268e8df62f
Jinli Duan <jinlid@uci.edu>
2023-09-25 09:21:05 -0400

    Update autopack.py

--- autopack.py
+++ autopack.py
@@ -1,6 +1,24 @@
 import subprocess
 import datetime
 import os
+from collections import deque
+
+class PodQueue:
+    def __init__(self):
+        self.queue = deque()
+
+    def add_pod(self, pod_name):
+        if self.queue:
+            # if not empty,set last one as producer
+            producer_pod = self.queue[-1]
+            print(f"{producer_pod} is set as producer.")
+
+        # adding new pod in the queue
+        self.queue.append(pod_name)
+        print(f"{pod_name} is added to the queue as consumer.")
+
+    def get_consumer(self):
+        return self.queue[0] if self.queue else None
 
 def auto_pack(command, file):
     full_command = command + [file]
@@ -23,10 +41,10 @@ def auto_pack(command, file):
 
 def auto_unpack_and_dockerize(rpz_file):
     container_name = f"container_{rpz_file.replace('/', '_')}_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}"
-    
+
     try:
         result = subprocess.run(["reprounzip", "docker", "setup", rpz_file, container_name], check=True, stdout=subprocess.PIPE, text=True)
-        
+
         # Extract the docker image name from the command output
         for line in result.stdout.split("\n"):
             if "Docker image" in line:
@@ -40,6 +58,7 @@ def auto_unpack_and_dockerize(rpz_file):
     print(f"Unpacking and dockerization completed for {rpz_file}. Container name: {container_name}")
 
 if __name__ == "__main__":
+    pod_queue = PodQueue()
     directory = input("Enter the directory containing the files you want to trace, pack, unpack, and dockerize: ")
 
     # Mapping of file extensions to commands
@@ -59,4 +78,6 @@ if __name__ == "__main__":
                 full_path = os.path.join(root, file)
                 rpz_name = auto_pack(commands[ext], full_path)
                 if rpz_name:
-                    auto_unpack_and_dockerize(rpz_name)
+                    container_name = auto_unpack_and_dockerize(rpz_name)
+                    if container_name:
+                        pod_queue.add_pod(container_name)
